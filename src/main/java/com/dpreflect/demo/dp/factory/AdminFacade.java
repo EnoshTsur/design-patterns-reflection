707a03e5fd8489522da977e3d041908670849095
EnoshTs <enosh.tsur@haaretz.co.il>
2020-09-15 18:32:06 +0300

    more reflection

--- src/main/java/com/dpreflect/demo/dp/factory/AdminFacade.java
+++ src/main/java/com/dpreflect/demo/dp/factory/AdminFacade.java
@@ -5,10 +5,20 @@ import javax.swing.*;
 
 public class AdminFacade implements ClientFacade {
 
+    private String adminName = "Jacob";
+
     @Override
     public AdminFacade login(String email, String password) {
         return !(email.equals("admin") && password.equals("123")) ? null :
                         new AdminFacade();
+    }
+
+    public String hello(String name){
+        return "hello " + name;
+    }
+
+    private void secret(){
+        System.out.println("Private!!! :)");
     }
 
 }
--- src/main/java/com/dpreflect/demo/ref/Main.java
+++ src/main/java/com/dpreflect/demo/ref/Main.java
@@ -2,21 +2,28 @@ package com.dpreflect.demo.ref;
 
 import com.dpreflect.demo.dp.factory.AdminFacade;
 
-import java.lang.reflect.Constructor;
+
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 final class A {
 
 }
 
+
 public class Main {
 
+    private String name;
+
+    private String getName(){ return  name; }
 
     public static void main(String[] args) {
 
         Class<AdminFacade> adminFacadeClass = AdminFacade.class;
+
         Class<A> aClass = A.class;
 
 //        System.out.println(
@@ -27,9 +34,11 @@ public class Main {
 //                adminFacadeClass.getSimpleName()
 //        );
 
+        // 3
 //        int modifierStatus = aClass.getModifiers();
+//
 //        System.out.println(
-//                Modifier.isFinal(modifierStatus)
+//                Modifier.isAbstract(modifierStatus)
 //        );
 
 //        Class<?>[] interfaces = adminFacadeClass.getInterfaces();
@@ -42,10 +51,30 @@ public class Main {
 //                superclass.getSimpleName()
 //        );
 
-//        Method[] methods = adminFacadeClass.getMethods();
+//        Method[] methods = adminFacadeClass.getDeclaredMethods();
 //        Stream.of(methods)
 //                .map(Method::getName)
 //                .forEach(System.out::println);
+
+        AdminFacade adminFacade = new AdminFacade();
+
+
+
+//        try {
+//
+//            Method hello = AdminFacade.class.getMethod("hello", String.class);
+//            Method secret = AdminFacade.class.getDeclaredMethod("secret");
+//
+//            secret.setAccessible(true);
+//            secret.invoke(adminFacade, null);
+//
+//            System.out.println(
+//                    Arrays.toString(hello.getParameterTypes()) + "\n" + hello.getParameterCount()
+//            );
+//
+//        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+//            e.printStackTrace();
+//        }
 
 
     }
